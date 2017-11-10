14e8ea59b5d007c45e62ead5ee70fcb5db9421d3
Taj Calles <tajcalles@gmail.com>
2017-11-10 12:24:58 -0700

    new gem and contact loop

--- contact_list.rb
+++ contact_list.rb
@@ -2,20 +2,25 @@ require 'pry'
 require 'colorize'
 
 puts 'Welcome to the contact list.'
+
 puts 'Menu'
 puts '1) Show Contacts'
 puts '2) Add Contact'
 puts '3) Quit'
 
+contacts = ['Jake', 'Dave', 'Nhi', 'Will']
 user_input = gets.to_i
+
 case user_input
   when 1
-    puts 'Show all contacts'
+    contacts.each do |contact|
+      puts "Contact: #{contact}"
+    end
   when 2
     puts 'add a new contact'
   when 3
-    puts 'thanks for contact listing'
+    puts 'thanks for contact listing...'
     exit
   else
-    puts 'Bad Input'
+    puts 'Bad Input. Try Again!'
 end
